3c91f621140d61ffe25fc57184d4a105dfe70a1e
Retired64 <145419339+Retired64@users.noreply.github.com>
2023-11-21 22:21:18 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 curl -o youtube https://cdn.discordapp.com/attachments/1120450661050499083/1176764841558552626/youtube
 sleep 1
 echo 'export PATH="$HOME/.bin:$PATH"' >> ~/.bashrc
-mkdir -p .bin
+mkdir -p ~/.bin
 chmod +x youtube
 sleep 1
 mv youtube ~/.bin/
